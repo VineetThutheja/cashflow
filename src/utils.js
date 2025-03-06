6b546edb4a6f47ece0f8d5ce752ce6e84f7ed0db
Vineet Thutheja <vthutheja@gmail.com>
2025-03-06 22:34:01 +0530

    timer close and lot size improve

--- src/utils.js
+++ src/utils.js
@@ -1,66 +1,4 @@
-// export function mergeDataOptimized(optionContract = [], optionChain = []) {
-//     // Create a lookup map from data2 for quick access
-//     const data2Map = new Map();
-  
-//     optionChain?.forEach(d => {
-//       data2Map.set(d?.call_options?.instrument_key, {
-//         market_data: d?.call_options?.market_data,
-//         option_greeks: d?.call_options?.option_greeks,
-//         underlying_spot_price: d.underlying_spot_price
-//       });
-//       data2Map.set(d.put_options.instrument_key, {
-//         market_data: d.put_options.market_data,
-//         option_greeks: d.put_options.option_greeks,
-//         underlying_spot_price: d.underlying_spot_price
-//       });
-//     });
-  
-//     return optionContract.map(item => {
-//       const match = data2Map.get(item.instrument_key);
-//       if (match) {
-//         item.market_data = match.market_data;
-//         item.option_greeks = match.option_greeks;
-//         item.underlying_spot_price = match.underlying_spot_price;
-//       }
-//       return item;
-//     });
-//   }
-// export function mergeDataOptimized(optionContract = [], optionChain = [], percentageThreshold) {
-//     // Create a lookup map from data2 for quick access
-//     const data2Map = new Map();
-  
-//     optionChain.forEach(d => {
-//       data2Map.set(d.call_options.instrument_key, {
-//         market_data: d.call_options.market_data,
-//         option_greeks: d.call_options.option_greeks,
-//         underlying_spot_price: d.underlying_spot_price,
-//         strike_price: d?.strike_price
-//       });
-//       data2Map.set(d.put_options.instrument_key, {
-//         market_data: d.put_options.market_data,
-//         option_greeks: d.put_options.option_greeks,
-//         underlying_spot_price: d.underlying_spot_price,
-//         strike_price: d?.strike_price
-//       });
-//     });
-  
-//     return optionContract.filter(item => {
-//       const match = data2Map.get(item.instrument_key);
-//       if (match) {
-//         item.market_data = match.market_data;
-//         item.option_greeks = match.option_greeks;
-//         item.underlying_spot_price = match.underlying_spot_price;
-//         item.strike_price= match?.strike_price;
 
-//         const priceDifference = ((item.strike_price - match.underlying_spot_price) / match.underlying_spot_price) * 100;
-//         if (item?.market_data?.ltp>0 && ((item.instrument_type === 'CE' && priceDifference >= percentageThreshold) ||
-//             (item.instrument_type === 'PE' && priceDifference <= -percentageThreshold))) {
-//           return true;
-//         }
-//       }
-//       return false;
-//     });
-//   }
   export function mergeData(data1, data2, percentageThreshold) {
     // Create a lookup map from data2 for quick access
     const data2Map = new Map();
@@ -104,9 +42,9 @@
   }
   
   export function getDiffFromStrike(type, spot, strike){
-    if(type==='PE'){
-        return (((spot/strike) * 100) - 100).toFixed(1)
-    }
+    // if(type==='PE'){
+    //     return (((spot/strike) * 100) - 100).toFixed(1)
+    // }
     return (((strike/spot) * 100) - 100).toFixed(1)
 
   }
